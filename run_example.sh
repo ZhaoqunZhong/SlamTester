893550d7e562c806a4dff9b39d38795f629a7453
ZhaoqunZhong <zhongzhaoqun1991@outlook.com>
2023-02-01 15:31:00 +0800

    small refactor

--- run_example.sh
+++ run_example.sh
@@ -1,1 +1,13 @@
-./build/SlamTester -algorithm=vins_mono -camConfig=/Users/zhongzhaoqun/Downloads/Euroc/camera_euroc.txt -dataBag=/Users/zhongzhaoqun/Downloads/MH-01-easy/MH_01_easy.bag -dataset=euroc -groundTruth=/Users/zhongzhaoqun/Downloads/MH-01-easy/state_groundtruth_estimate0/data.csv -ciExtrinsic=/Users/zhongzhaoqun/Downloads/MH-01-easy/cam0/sensor.yaml -resizeAndUndistort=false -algoConfig=/Users/zhongzhaoqun/Downloads/Euroc/euroc_config.yaml -skipCamMsgIfLag=false -showOrigCamStream=true -viewer=pangolin -socketConfig=/Users/zhongzhaoqun/CLionProjects/SlamTester/socket_publisher/config.yaml 
+./build/SlamTester \
+-algorithm=dso \
+-camConfig=/Users/zhongzhaoqun/Downloads/Euroc/camera_euroc.txt \
+-dataBag=/Users/zhongzhaoqun/Downloads/MH-01-easy/MH_01_easy.bag \
+-dataset=euroc \
+-groundTruth=/Users/zhongzhaoqun/Downloads/MH-01-easy/state_groundtruth_estimate0/data.csv \
+-ciExtrinsic=/Users/zhongzhaoqun/Downloads/MH-01-easy/cam0/sensor.yaml \
+-resizeAndUndistort=true \
+-algoConfig=/Users/zhongzhaoqun/Downloads/Euroc/euroc_config.yaml \
+-skipCamMsgIfLag=false \
+-showOrigCamStream=true \
+-viewer=socket \
+-socketConfig=/Users/zhongzhaoqun/CLionProjects/SlamTester/socket_publisher/config.yaml 
---
+++ viewer/readme.txt
@@ -0,0 +1,8 @@
+Original files:
+Dockerfile  app.js  package.json  public  views
+
+run:
+npm install
+
+then run:
+node app.js
